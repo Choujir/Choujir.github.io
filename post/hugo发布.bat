449d92a39def0c21d070e131494ba3690a1a42b9
choujir <1947037835@qq.com>
2023-06-17 16:48:00 +0800

    Add a new Hugo post

--- post/hugo发布.bat
+++ post/hugo发布.bat
@@ -4,14 +4,12 @@ cd /dP:\Hugo_Blog
 hugo -t clarity
 
 cd public
+
 git remote add origin git@github.com:Choujir/Choujir.github.io.git
 cat .git/config
 git status
 git add .
 git commit -m "Add a new Hugo post"
 git push -u origin master
-
-
-
 
 start https://choujir.github.io/
